c82dd912f1ea653746bfafe117a13e80c7a5703c
Vishwam Shukla <shuklavishwam21@gmail.com>
2020-07-29 13:31:25 +0530

    minor changes

--- app/src/main/java/com/example/civilagency/HomeActivity.java
+++ app/src/main/java/com/example/civilagency/HomeActivity.java
@@ -112,7 +112,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         final TextView userNameTextView = headerView.findViewById(R.id.name_textView);
         final CircleImageView profileImageView = headerView.findViewById(R.id.nav_header_profile_imageView);
 
-        UserRef = FirebaseDatabase.getInstance().getReference("Individual Reports");
+        UserRef = FirebaseDatabase.getInstance().getReference("Users").child("Civil").child(currentUserID);
 
         UserRef.addValueEventListener(new ValueEventListener() {
             @Override
